cc24868021c0bd3578fbb3e16c68f2fc0cf1bb8d
joao40sa <50167424+joao40sa@users.noreply.github.com>
2021-03-19 10:55:28 +0000

    Delete Docente.java

--- Docente.java
+++
@@ -1,8 +0,0 @@
-class Docente extends Pessoa{
-
-	public Docente(String nome, String password, String departamento, String faculdade, String contacto, String morada, int numero, String validade_cc)
-	{
-		super(nome, password, departamento, faculdade, contacto, morada, numero, validade_cc);
-	}
-
-}
