8840ed19cc54d6537e069336333a27a83d4ea910
joao40sa <50167424+joao40sa@users.noreply.github.com>
2021-03-19 10:55:17 +0000

    Delete Estudante.java

--- Estudante.java
+++
@@ -1,8 +0,0 @@
-class Estudante extends Pessoa{
-
-	public Estudante(String nome, String password, String departamento, String faculdade, String contacto, String morada, int numero, String validade_cc)
-	{
-		super(nome, password, departamento, faculdade, contacto, morada, numero, validade_cc);
-	}
-
-}
