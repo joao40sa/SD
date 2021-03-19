61013c0dea7c777b1fc90f32e5ae173bd60c87ad
joao40sa <50167424+joao40sa@users.noreply.github.com>
2021-03-19 10:55:11 +0000

    Delete Funcionario.java

--- Funcionario.java
+++
@@ -1,8 +0,0 @@
-class Funcionario extends Pessoa{
-
-	public Funcionario(String nome, String password, String departamento, String faculdade, String contacto, String morada, int numero, String validade_cc)
-	{
-		super(nome, password, departamento, faculdade, contacto, morada, numero, validade_cc);
-	}
-
-}
